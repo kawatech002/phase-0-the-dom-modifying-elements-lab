d52fd7125eff088bfb9642eef39a6407e9f0a651
kawatech002 <kawanjiku94@gmail.com>
2023-10-11 05:20:02 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+main.remove()
+
+//let victory= document.createElement('h1')
+//victory.id = 'victory'
+//console.log(victory)
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "YOUR-NAME is the champion";
